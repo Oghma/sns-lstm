3a9fe498cfdecd1d20036f5573fa2af9f4f145ea
Matteo Lisotto <matteo.lisotto@gmail.com>
2018-10-27 16:21:47 +0200

    Add the coordinates helper functions

---
+++ src/coordinates_helpers.py
@@ -0,0 +1,41 @@
+"""Module that defines the coordinates helper. An helper provides the correct
+coordinates according to the traininig or sampling phase and the time-step."""
+
+
+def train_helper(step, coordinates_gt, *args):
+    """Helper used in training phase. Returns the ground truth coordinates.
+
+    Args:
+      step: int. The current time-step.
+      coordinates_gt: tensor of shape [max_num_ped, 2]. The ground truth
+        coordinates.
+
+    Returns:
+      In training phase always returns the ground truth coordinates.
+
+    """
+    return coordinates_gt
+
+
+def sample_helper(obs_len):
+    """Helper used in sampling phase. Returns a function that returns the ground
+    truth coordinates or the predicted coordinates based on the value of step.
+
+    In sampling phase, if the step variable is less of the obs_len, the helper
+    returns the ground truth coordinates. Otherwise returns the predicted
+    coordinates.
+
+    Args:
+      obs_len: int. The number of time-step to be observed.
+
+    Returns:
+      A function that receives in input the time-step, the ground truth
+        coordinates and the predicted coordinates and returns the right
+        coordinates according to the time-step.
+
+    """
+
+    def helper(step, coordinates_gt, coordinates_predicted):
+        return coordinates_predicted if step >= obs_len else coordinates_gt
+
+    return helper
