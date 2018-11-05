033b9333a12021e2add3def60dbe32cfcd9ff08d
Matteo Lisotto <matteo.lisotto@gmail.com>
2018-11-05 12:01:58 +0100

    Add the evaluation functions

---
+++ src/utils/evaluation.py
@@ -0,0 +1,68 @@
+"""Module that defines the metrics used for evaluation."""
+
+import tensorflow as tf
+
+
+def average_displacement_error(coordinates_predicted, coordinates_gt, num_peds):
+    """Function that calculates the average displacement error.
+
+    The formula is
+    sqrt(sum((coordinates_gt - coordinates_predicted)^2)) / (num_peds * pred_length)
+
+    Args:
+      coordinates_predicted: tensor of shape [max_num_ped, pred_length, 2].
+        Tensor that contains the coordinates predicted from the model.
+      coordinates_gt: tensor of shape [pred_length, max_num_ped, 2]. Tensor that
+        contains the ground truth coordinates.
+      num_peds: tensor of shape []. Number of pedestrians that are in the
+        sequence.
+
+    Returns:
+      tensor of shape [] containing the average displacement error of the
+        pedestrians that are in the sequence.
+    """
+    i = tf.constant(0)
+    ade = tf.constant(0, tf.float32)
+
+    cond = lambda i, ade: tf.less(i, num_peds)
+
+    def body(i, ade):
+        ade_ped = coordinates_gt[i] - coordinates_predicted[:, i]
+        ade_ped = tf.norm(ade_ped)
+        return tf.add(i, 1), tf.add(ade, ade_ped)
+
+    _, ade = tf.while_loop(cond, body, [i, ade])
+    return ade / tf.cast(num_peds * coordinates_gt.shape[1], tf.float32)
+
+
+def final_displacement_error(coordinates_predicted, coordinates_gt, num_peds):
+    """Function that calculates the final displacement error.
+
+    The formula is
+    sqrt(sum((coordinates_gt - coordinates_predicted)^2)) / num_peds
+
+    Args:
+      coordinates_predicted: tensor of shape [max_num_ped, 2]. Tensor that
+        contains the coordinates predicted from the model.
+      coordinates_gt: tensor of shape [max_num_ped, 2]. Tensor that contains the
+        ground truth coordinates.
+      num_peds: tensor of shape []. Number of pedestrians that are in the
+        sequence.
+
+    Returns:
+      tensor of shape [] containing the final displacement error of the
+        pedestrians that are in the sequence.
+
+    """
+    i = tf.constant(0)
+    fde = tf.constant(0, tf.float32)
+
+    cond = lambda i, fde: tf.less(i, num_peds)
+
+    def body(i, fde):
+        fde_ped = coordinates_gt[i] - coordinates_predicted[i]
+        fde_ped = tf.norm(fde_ped)
+        return tf.add(i, 1), tf.add(fde, fde_ped)
+
+    _, fde = tf.while_loop(cond, body, [i, fde])
+    return fde / tf.cast(num_peds, tf.float32)
