085e321b6ba15a6c596ca4dce5e06253cf042362
Matteo Lisotto <matteo.lisotto@gmail.com>
2018-12-08 19:00:18 +0100

    Do not include peds that are standing all time

--- src/utils/loader.py
+++ src/utils/loader.py
@@ -167,7 +167,12 @@ class DataLoader:
                 for ped in peds:
                     # Get the frames where ped appear
                     frames = sequence[sequence[:, 1] == ped, :]
-                    if frames.shape[0] == self.trajectory_size:
+                    # Check that the pedestrian is not standing all time
+                    if (
+                        frames.shape[0] == self.trajectory_size
+                        and not np.all(frames[:, 2] == frames[0, 2])
+                        and not np.all(frames[:, 3] == frames[0, 3])
+                    ):
                         traj_frame.append(frames)
                 # If no trajectory is long enough traj_frame is empty
                 if traj_frame:
