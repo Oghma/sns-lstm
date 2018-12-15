7292f7885a17bea6c5d4dd6fa72a6111c3fadc4a
Matteo Lisotto <matteo.lisotto@gmail.com>
2018-12-15 15:58:06 +0100

    Generate correlated coordinates

--- src/position_estimates.py
+++ src/position_estimates.py
@@ -17,8 +17,8 @@ def social_train_position_estimate(cell_output, coordinates_gt, output_size, *ar
       tuple containing a tensor of shape [max_num_ped, 2] that contains the pdf.
 
     """
-    # Calculate the new coordinates based on Graves (2013) equations. Assume a
-    # bivariate Gaussian distribution.
+    # Calculate the probability density function on Graves (2013) equations.
+    # Assume a bivariate Gaussian distribution.
     with tf.name_scope("Calculate_coordinates"):
         # Equations 20 - 22
         # Split and squeeze to have shape [max_num_ped]
@@ -129,21 +129,36 @@ def social_sample_position_estimate(
 
     """
 
+    # Calculate the new coordinates based on Graves (2013) equations. Assume a
+    # bivariate Gaussian distribution.
     with tf.name_scope("Calculate_coordinates"):
-        # Equations 20 - 22
+        # Equations 20 - 22 from Graves
         # Split and squeeze to have shape [max_num_ped]
         mu_x, mu_y, std_x, std_y, rho = list(
             map(lambda x: tf.squeeze(x, 1), tf.split(cell_output, output_size, 1))
         )
         std_x = tf.exp(std_x)
         std_y = tf.exp(std_y)
+        rho = tf.tanh(rho)
 
-        mu = tf.stack([mu_x, mu_y], 1)
-        std = tf.stack([std_x, std_y], 1)
+        # Kaiser-Dickman algorithm (Kaiser & Dickman, 1962)
+        # Generate two sample X1, X2 from the standard normal distribution (mu =
+        # 0, sigma = 1)
+        normal_coords = tf.random.normal(tf.TensorShape([mu_x.shape[0], 2]))
+        # Generate the correlation.
+        # correlation = rho * X1 + sqrt(1 - pow(rho)) * X2
+        correlation = (
+            rho * normal_coords[:, 0]
+            + tf.sqrt(1 - tf.square(rho)) * normal_coords[:, 1]
+        )
 
-        # Sample the coordinates
-        dist = tf.distributions.Normal(mu, std)
-        coordinates = dist.sample()
+        # Define the two coordinates correlated
+        # Y1 = mu_x + sigma_x * X1
+        # Y2 = mu_y + sigma_y * correlation
+        coords_x = mu_x + std_x * normal_coords[:, 0]
+        coords_y = mu_y + std_y * correlation
+
+        coordinates = tf.stack([coords_x, coords_y], 1)
         return coordinates, layer_output(coordinates)
 
 
