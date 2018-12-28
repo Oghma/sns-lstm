88035df04046bbfc2092fb51ab8629fecdc8d162
Matteo Lisotto <matteo.lisotto@gmail.com>
2018-12-28 12:07:33 +0100

    Move the settings of the logger in a separate file

--- scripts/sample.py
+++ scripts/sample.py
@@ -10,6 +10,7 @@ import tensorflow as tf
 
 import utils
 import pooling_layers
+from logger import setLogger
 from model import SocialModel
 from coordinates_helpers import sample_helper
 from losses import social_loss_function
@@ -17,50 +18,8 @@ from position_estimates import social_sample_position_estimate
 from beautifultable import BeautifulTable
 
 
-def logger(hparams, args):
-    log_file = hparams.name + "-train.log"
-    log_folder = None
-    level = "INFO"
-    formatter = logging.Formatter(
-        "[%(asctime)s %(filename)s] %(levelname)s: %(message)s"
-    )
 
-    # Check if you have to add a FileHandler
-    if args.logFolder is not None:
-        log_folder = args.logFolder
-    elif hparams.logFolder is not None:
-        log_folder = hparams.logFolder
-
-    if log_folder is not None:
-        log_file = os.path.join(log_folder, log_file)
-        if not os.path.exists(log_folder):
-            os.makedirs(log_folder)
-
-    # Set the level
-    if args.logLevel is not None:
-        level = args.logLevel.upper()
-    elif hparams.logLeve is not None:
-        level = hparams.logLevel.upper()
-
-    # Get the logger
-    logger = logging.getLogger()
-    # Remove handlers added previously
-    for handler in logger.handlers[:]:
-        if isinstance(handler, logging.FileHandler):
-            handler.close()
-        logger.removeHandler(handler)
-    if log_folder is not None:
-        # Add a FileHandler
-        file_handler = logging.FileHandler(log_file)
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
-    # Add a StreamHandler that display on sys.stderr
-    stderr_handler = logging.StreamHandler()
-    stderr_handler.setFormatter(formatter)
-    logger.addHandler(stderr_handler)
-
-    # Set the level
-    logger.setLevel(level)
+PHASE = "SAMPLE"
 
 
 def main():
@@ -111,7 +70,7 @@ def main():
         # Load the parameters
         hparams = utils.YParams(experiment)
         # Define the logger
-        logger(hparams, args)
+        setLogger(hparams, args, PHASE)
 
         remainSpaces = 29 - len(hparams.name)
         logging.info(
